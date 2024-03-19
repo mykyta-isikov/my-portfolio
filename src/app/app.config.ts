b9bdcbf662b3aabef2b6b6ce357cf4385da06b53
Mykyta Isikov <mykyta.isikov@gmail.com>
2024-03-20 01:20:37 +0200

    feat(my-portfolio): setup @angular/firebase deployment
    - Installed and configured @angular/firebase package.
    - Updated website title to "Isikov".

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAngularSvgIcon } from 'angular-svg-icon';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +15,6 @@ export const appConfig: ApplicationConfig = {
       })
     ),
     provideHttpClient(),
-    provideAngularSvgIcon()
+    provideAngularSvgIcon(), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"my-portfolio-isikov","appId":"1:974777437538:web:8ec75b531f2b9e556f703b","storageBucket":"my-portfolio-isikov.appspot.com","apiKey":"AIzaSyACZMyJgXdfpAuWcoxE-PpVaJLl2lvd-R4","authDomain":"my-portfolio-isikov.firebaseapp.com","messagingSenderId":"974777437538","measurementId":"G-T1RS835F5H"}))), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService
   ]
 };
